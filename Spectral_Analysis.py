86389f9b0f60d8109c69d91f1ef70bd1d7484816
Janna May <a.jannamay@wustl.edu>
2024-03-02 21:24:10 -0600

    generate_normSpectrum: recover intensity info option on uncombined CHs

--- Spectral_Analysis.py
+++ Spectral_Analysis.py
@@ -136,6 +136,7 @@ class Spectra:
                 values_along_CH = self.Raw_data[zstack_ind, indx_pixorg[0][ind], indx_pixorg[1][ind], :]
                 # Calculate the definite integral of the curve
                 area_under_curve = simps(values_along_CH, self.xvalues_nm)
+                # Normalize the curve by dividing each data point by the area under the curve
                 norm_values_along_CH = values_along_CH / area_under_curve
                 values.append(norm_values_along_CH)
                 area_under_curve_list.append(area_under_curve)
@@ -143,7 +144,7 @@ class Spectra:
             # Calculating mean and std
             meanI = np.mean(area_under_curve_list, axis=0)
 
-            # Normalize the curve by dividing each data point by the area under the curve
+            # Retain intensity profile comparisons
             for ind in range(len(values)):
                 values_norm_Iinfo = values[ind]*meanI
                 values_norm_Iinfo_list.append(values_norm_Iinfo)
@@ -235,17 +236,15 @@ class Spectra_no_z:
                 rescale=False
             )
             self.Unmixed_data[:, :, :] = image_temp
-
-            # Specify array of organelle
-            self.arr_organelle1 = self.Unmixed_data[:, :, 0]
-            self.arr_organelle2 = self.Unmixed_data[:, :, 1]
-
+            
         # x-axis for the spectrum later on
         self.xaxis = [f'{a+1} ({self.start_nm + self.bin_size * a})' for a in range(self.Num_CH)]
         self.xvalues = np.array([a+1 for a in range(self.Num_CH)])
         self.xvalues_nm = np.array([self.start_nm + self.bin_size * a for a in range(self.Num_CH)])
+    
 
-    def generate_scatter_plot_varNumpixels(self,saturated_pix, Num_toppixels):
+    # Generate scatter plot
+    def generate_scatter_plot(self,saturated_pix, Num_toppixels):
         def select_pix(array1, array2):
             # Create a boolean mask where the condition is true for elements in array2 equal to 0
             mask = (array2 == 0)
@@ -264,17 +263,21 @@ class Spectra_no_z:
             indices = np.where(condition)
             return indices
         
+        # Specify array of organelle
+        arr_organelle1 = self.Unmixed_data[:, :, 0]
+        arr_organelle2 = self.Unmixed_data[:, :, 1]
+        
         if saturated_pix == False:
             # Location of saturated pixels
-            ind_saturated = np.where(self.Raw_data >= 4050)
+            ind_saturated = np.where(self.Raw_data[:,:,:] >= 4050)
             # Mask for the saturated pixels
-            mask_saturated =  np.ones(self.arr_organelle1.shape)
+            mask_saturated =  np.ones(arr_organelle1.shape)
             # Set saturated to zero across all channels
             mask_saturated[ind_saturated[0],ind_saturated[1]] = 0
             
             # Apply to unmixed data
-            arr_organelle1_nosat = self.arr_organelle1*mask_saturated
-            arr_organelle2_nosat = self.arr_organelle2*mask_saturated
+            arr_organelle1_nosat = arr_organelle1*mask_saturated
+            arr_organelle2_nosat = arr_organelle2*mask_saturated
             
             # Pixels that we are sure of to be in a certain organelle (based from unmixed data)
             organelle1_pix = select_pix(arr_organelle1_nosat, arr_organelle2_nosat)
@@ -294,24 +297,120 @@ class Spectra_no_z:
                         label=f'${self.organelle2} \geq {np.min(organelle2_pix)}, {self.organelle1} = 0$')
         else:
             # Pixels that we are sure of to be in a certain organelle (based from unmixed data)
-            organelle1_pix = select_pix(self.arr_organelle1, self.arr_organelle2)
-            organelle2_pix = select_pix(self.arr_organelle2, self.arr_organelle1)
+            organelle1_pix = select_pix(arr_organelle1, arr_organelle2)
+            organelle2_pix = select_pix(arr_organelle2, arr_organelle1)
 
             # Find indices of points that satisfy the condition
-            indx_pixorg1 = indx_select_pix(self.arr_organelle1, self.arr_organelle2, organelle1_pix)
-            indx_pixorg2 = indx_select_pix(self.arr_organelle2, self.arr_organelle1, organelle2_pix)
+            indx_pixorg1 = indx_select_pix(arr_organelle1, arr_organelle2, organelle1_pix)
+            indx_pixorg2 = indx_select_pix(arr_organelle2, arr_organelle1, organelle2_pix)
 
             # Plot the 2 arrays in a scatter plot
-            plt.scatter(self.arr_organelle2, self.arr_organelle1, alpha=0.3)
+            plt.scatter(arr_organelle2, arr_organelle1, alpha=0.3)
 
             # Highlight the selected point
-            plt.scatter(self.arr_organelle2[indx_pixorg1], self.arr_organelle1[indx_pixorg1], color='red',
+            plt.scatter(arr_organelle2[indx_pixorg1], arr_organelle1[indx_pixorg1], color='red',
                         label=f'${self.organelle2} = 0, {self.organelle1} \geq {np.min(indx_pixorg1)}$')
-            plt.scatter(self.arr_organelle2[indx_pixorg2], self.arr_organelle1[indx_pixorg2], color='green',
+            plt.scatter(arr_organelle2[indx_pixorg2], arr_organelle1[indx_pixorg2], color='green',
                         label=f'${self.organelle2} \geq {np.min(indx_pixorg2)}, {self.organelle1} = 0$')
+            
 
-        # Add labels
+        # Add labels and title
         plt.ylabel(f'Pixel intensity ({self.organelle1})')
         plt.xlabel(f'Pixel intensity ({self.organelle2})')
 
+
         return plt, indx_pixorg1, indx_pixorg2
+
+
+    # Generate spectrum plot of chosen pixels
+    def generate_normSpectrum(self, indx_pixorg, Num_CHtocombi, withIinfo):
+        if Num_CHtocombi == 0: # if you don't combine CHs
+            values = []
+
+            if withIinfo == True:
+                area_under_curve_list = []
+                values_norm_Iinfo_list = []
+                for ind in range(len(indx_pixorg[0])):
+                    values_along_CH = self.Raw_data[indx_pixorg[0][ind], indx_pixorg[1][ind], :]
+                    # Calculate the definite integral of the curve
+                    area_under_curve = simps(values_along_CH, self.xvalues_nm)
+                    # Normalize the curve by dividing each data point by the area under the curve
+                    norm_values_along_CH = values_along_CH / area_under_curve
+                    values.append(norm_values_along_CH)
+                    area_under_curve_list.append(area_under_curve)
+
+                # Calculating mean area under the curve to recover I info
+                meanI = np.mean(area_under_curve_list, axis=0)
+
+                # Recover I info
+                for ind in range(len(values)):
+                    values_norm_Iinfo = values[ind]*meanI
+                    values_norm_Iinfo_list.append(values_norm_Iinfo)
+
+                    plt.plot(self.xaxis, values_norm_Iinfo, color='gray', marker='o')
+                    plt.xticks(self.xaxis, rotation=90)
+                    plt.xlabel(r'CH # ($\lambda$ in nm)')
+
+                # Calculating mean and std
+                mean = np.mean(values_norm_Iinfo_list, axis=0)
+                std = np.std(values_norm_Iinfo_list, axis=0, ddof=1)
+            else: 
+                values_along_CH = self.Raw_data[indx_pixorg[0][ind], indx_pixorg[1][ind], :]
+                area_under_curve = simps(values_along_CH, self.xvalues_nm)
+                norm_values_along_CH = values_along_CH / area_under_curve
+                values.append(norm_values_along_CH)
+
+                plt.plot(self.xaxis, values_norm_Iinfo, color='gray', marker='o')
+                plt.xticks(self.xaxis, rotation=90)
+                plt.xlabel(r'CH # ($\lambda$ in nm)')
+
+                # Calculating mean and std
+                mean = np.mean(values, axis=0)
+                std = np.std(values, axis=0, ddof=1)
+
+            
+            # Plotting mean and std
+            plt.plot(self.xaxis, mean, color='#CC4F1B', marker='o',label='Mean')
+            plt.fill_between(self.xaxis, mean-std, mean+std,
+                alpha=0.5, edgecolor='#CC4F1B', facecolor='#FF9848',label='Std')
+        else:
+            values = []
+            for ind in range(len(indx_pixorg[0])):
+                values_along_CH = self.Raw_data[indx_pixorg[0][ind], indx_pixorg[1][ind], :]
+                consecutive_means = []
+                combi_xvalues_nm = []
+                combi_CH = []
+                for i in range(0, len(values_along_CH) - 1, Num_CHtocombi):
+                    mean_value = np.mean(values_along_CH[i:i+Num_CHtocombi])
+                    consecutive_means.append(mean_value)
+                    combi_xvalues_nm.append(np.mean(self.xvalues_nm[i:i+Num_CHtocombi]))
+                    combi_CH.append(np.array(range(self.Num_CH))[i:i+Num_CHtocombi] + 1)
+
+                consecutive_means = np.asarray(consecutive_means)
+                combi_xvalues_nm = np.asarray(combi_xvalues_nm)
+                # Calculate the definite integral of the curve
+                area_under_curve = simps(consecutive_means, combi_xvalues_nm)
+                # Normalize the curve by dividing each data point by the area under the curve
+                norm_consecutive_means = consecutive_means / area_under_curve
+                values.append(norm_consecutive_means)
+                # xvalues of combined CHs
+                combi_xvalues = [f'{combi_CH[a][0]}-{combi_CH[a][-1]} ({combi_xvalues_nm[a]})' for a in range(len(combi_xvalues_nm))]
+
+                plt.plot(combi_xvalues, norm_consecutive_means, color='gray', marker='o')
+                plt.xticks(rotation=90)
+                plt.xlabel(r'CH # ($\lambda$ in nm)')
+
+            # Plotting mean and std
+            mean = np.mean(values, axis=0)
+            std = np.std(values, axis=0, ddof=1)
+
+            plt.plot(combi_xvalues, mean, color='#CC4F1B', marker='o',label='Mean')
+            plt.fill_between(combi_xvalues, mean-std, mean+std,
+                alpha=0.5, edgecolor='#CC4F1B', facecolor='#FF9848',label='Std')
+        
+        # Other plot features
+        plt.ylim(bottom=0)
+        # Adjust layout to prevent overlapping
+        plt.tight_layout()
+        
+        return plt
